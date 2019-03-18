466fc3fe69ef5c31a66f6f5a5ef4abbd96bca451
Roche Periyanayagam <rocheinside@gmail.com>
2019-03-18 14:58:22 +0530

    backup result directory if exists and delete it

--- sheets/convert_sheets_to_files.py
+++ sheets/convert_sheets_to_files.py
@@ -1,12 +1,17 @@
 import xlrd
 import xlwt
 import os
+import shutil
+import time
 
 current_dir=os.getcwd()
 result_dir = current_dir + "/result"
+if(os.path.isdir(result_dir)):
+    shutil.copytree(result_dir,result_dir+str(int(time.time())))
+    shutil.rmtree(result_dir)
 os.mkdir(result_dir)
 
-workbook_name='empty_url.xlsx'
+workbook_name='upload_test.xlsx'
 my_workbook=xlrd.open_workbook(workbook_name)
 sheets = my_workbook.sheets()
 
