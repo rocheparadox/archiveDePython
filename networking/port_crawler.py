846924f4f48668bb785766c0b52ed801ca12ca1d
rocheparadox <rocheinside@gmail.com>
2020-02-04 21:38:52 +0530

    port crawler multithreaded

--- networking/port_crawler.py
+++ networking/port_crawler.py
@@ -3,6 +3,7 @@
 
 import socket
 import sys, os
+import threading
 
 def is_host_alive(hostname):
 
@@ -14,6 +15,21 @@ def is_host_alive(hostname):
     else:
         return False
 
+def crawl_ports(start, end):
+    sock = socket.socket()
+    for port in range(start, end):
+
+        # sys.stdout.write("Checking port {} \r".format(port))
+        # sys.stdout.flush()
+
+        try:
+            sock.connect((host, port))
+            print("port {} is listening ".format(port))
+            sock.close()
+            sock = socket.socket()
+        except Exception as exc:
+            pass
+
 
 if len(sys.argv) > 1:
     host = sys.argv[1]
@@ -24,19 +40,21 @@ if not is_host_alive(host):
     print("Host is not pingable")
     exit()
 
-sock = socket.socket()
-for port in range(1,65535):
 
-    sys.stdout.write("Checking port {} \r".format(port))
-    sys.stdout.flush()
+port_crawler_threads = []
 
-    try:
-        sock.connect((host, port))
-        print("port {} is listening ".format(port))
-        sock.close()
-        sock = socket.socket()
-    except Exception as exc:
-        pass
+for i in range(0,12):
+    start = (i * 5000) + 1
+    end = start + 5000
+    #print(start)
+    port_crawler_thread_temp = threading.Thread( target=crawl_ports, args=(start, end))
+    port_crawler_threads.append(port_crawler_thread_temp)
+
+port_crawler_thread_temp = threading.Thread( target=crawl_ports, args=(60000, 65535))
+port_crawler_threads.append(port_crawler_thread_temp)
+
+for port_crawler_thread in port_crawler_threads:
+    port_crawler_thread.start()
 
 
 
